3f1a14f42473022da02d96788f900d64b08f7d4b
Jcj <55246787@qq.com>
2022-09-19 00:06:13 +0800

    建立表格

--- src/views/achive/AchiveSearch.vue
+++ src/views/achive/AchiveSearch.vue
@@ -1,12 +1,60 @@
 <template>
-    <div>
-        <h1  style="font-size: 60px;color:crimson">业绩查询待开发</h1>
-    </div>
-    
-</template>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="Name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="Phone"
+        label="手机"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="Username"
+        label="用户名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="Namezh"
+        label="角色"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="Address"
+        label="地址"
+        width="280">
+
+      </el-table-column>
+    </el-table>
+  </template>
+
 <script>
 export default{
-    name :"AchiveSearch"
+    name :"AchieveSearch",
+    data(){
+        return {
+            tableData:[],
+        }
+    },
+    mounted(){
+        this.initUserInfo();
+    },
+    methods:{
+        initUserInfo(){
+            this.getRequest("/api/achieve/sy/info/search").then(resp=>{
+                if(resp){
+                    var arr = []
+                    arr.push(resp.data)
+                    this.tableData = arr
+                }
+            })
+        }
+    }
+
+
 }
 </script>
 <style scoped>
