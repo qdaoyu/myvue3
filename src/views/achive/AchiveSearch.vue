992e6161854734ae532e002134316fbe29740f78
Jcj <55246787@qq.com>
2022-09-19 08:47:03 +0800

    建立表格

--- src/views/achive/AchiveSearch.vue
+++ src/views/achive/AchiveSearch.vue
@@ -3,30 +3,159 @@
       :data="tableData"
       style="width: 100%">
       <el-table-column
-        prop="Name"
-        label="姓名"
-        width="180">
+        prop="Id"
+        label="序号"
+        width="100">
       </el-table-column>
       <el-table-column
-        prop="Phone"
-        label="手机"
-        width="180">
+        prop="Area"
+        label="区域"
+        width="100">
       </el-table-column>
       <el-table-column
-        prop="Username"
-        label="用户名"
-        width="180">
+        prop="Depart"
+        label="部门"
+        width="100">
       </el-table-column>
       <el-table-column
-        prop="Namezh"
-        label="角色"
-        width="180">
+        prop="Post"
+        label="岗位"
+        width="100">
       </el-table-column>
       <el-table-column
-        prop="Address"
-        label="地址"
-        width="280">
-
+        prop="Hireday"
+        label="入职日期"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="St_attendance"
+        label="标准出勤"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Ac_attendance"
+        label="实际出勤"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Restday"
+        label="休息"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Leaveday"
+        label="请假"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Absenteeism"
+        label="旷工"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Yq_close"
+        label="疫情闭店"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Overtime"
+        label="加班"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Business_travel"
+        label="出差"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Basesalary"
+        label="底薪"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Wx_achieve"
+        label="纹绣业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Pf_achieve"
+        label="皮肤业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Zc_achieve"
+        label="专场业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Gd_achieve"
+        label="光电业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Bs_achieve"
+        label="补色业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Shop_achieve"
+        label="门店业绩"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Wxpf_commission"
+        label="纹绣/皮肤提成"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Zc_commission"
+        label="专场提成"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Gd_commission"
+        label="光电提成"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Bs_commission"
+        label="补色提成"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="shop_commission"
+        label="门店提成"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Achievement"
+        label="绩效"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Subsidy"
+        label="补贴"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Handcost"
+        label="手工费"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Ac_basesalary"
+        label="实发底薪"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Deduction"
+        label="考勤扣款"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="Month_salary"
+        label="本月工资合计"
+        width="100">
       </el-table-column>
     </el-table>
   </template>
@@ -40,10 +169,10 @@ export default{
         }
     },
     mounted(){
-        this.initUserInfo();
+        this.initSyAchieveInfo();
     },
     methods:{
-        initUserInfo(){
+        initSyAchieveInfo(){
             this.getRequest("/api/achieve/sy/info/search").then(resp=>{
                 if(resp){
                     var arr = []
