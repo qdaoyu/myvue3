d516820f5388cfe2cce61b7815b4e44464b63429
qiudaoyu <552446787@qq.com>
2022-09-19 18:02:14 +0800

    无

--- src/views/achive/AchiveSearch.vue
+++ src/views/achive/AchiveSearch.vue
@@ -1,187 +1,97 @@
 <template>
-    <el-table
-      :data="tableData"
-      style="width: 100%">
-      <el-table-column
-        prop="Id"
-        label="序号"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Area"
-        label="区域"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Depart"
-        label="部门"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Post"
-        label="岗位"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Hireday"
-        label="入职日期"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="St_attendance"
-        label="标准出勤"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Ac_attendance"
-        label="实际出勤"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Restday"
-        label="休息"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Leaveday"
-        label="请假"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Absenteeism"
-        label="旷工"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Yq_close"
-        label="疫情闭店"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Overtime"
-        label="加班"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Business_travel"
-        label="出差"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Basesalary"
-        label="底薪"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Wx_achieve"
-        label="纹绣业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Pf_achieve"
-        label="皮肤业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Zc_achieve"
-        label="专场业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Gd_achieve"
-        label="光电业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Bs_achieve"
-        label="补色业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Shop_achieve"
-        label="门店业绩"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Wxpf_commission"
-        label="纹绣/皮肤提成"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Zc_commission"
-        label="专场提成"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Gd_commission"
-        label="光电提成"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Bs_commission"
-        label="补色提成"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="shop_commission"
-        label="门店提成"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Achievement"
-        label="绩效"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Subsidy"
-        label="补贴"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Handcost"
-        label="手工费"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Ac_basesalary"
-        label="实发底薪"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Deduction"
-        label="考勤扣款"
-        width="100">
-      </el-table-column>
-      <el-table-column
-        prop="Month_salary"
-        label="本月工资合计"
-        width="100">
-      </el-table-column>
-    </el-table>
-  </template>
+  <el-table :data="tableData" style="width: 100%"
+    height="750">
+    <el-table-column fixed prop="Id" label="序号" width="100">
+    </el-table-column>
+    <el-table-column prop="Area" label="区域" width="100">
+    </el-table-column>
+    <el-table-column prop="Depart" label="部门" width="100">
+    </el-table-column>
+    <el-table-column prop="Post" label="岗位" width="100">
+    </el-table-column>
+    <el-table-column prop="Hireday" label="入职日期" width="100">
+    </el-table-column>
+    <el-table-column prop="St_attendance" label="标准出勤" width="100">
+    </el-table-column>
+    <el-table-column prop="Ac_attendance" label="实际出勤" width="100">
+    </el-table-column>
+    <el-table-column prop="Restday" label="休息" width="100">
+    </el-table-column>
+    <el-table-column prop="Leaveday" label="请假" width="100">
+    </el-table-column>
+    <el-table-column prop="Absenteeism" label="旷工" width="100">
+    </el-table-column>
+    <el-table-column prop="Yq_close" label="疫情闭店" width="100">
+    </el-table-column>
+    <el-table-column prop="Overtime" label="加班" width="100">
+    </el-table-column>
+    <el-table-column prop="Business_travel" label="出差" width="100">
+    </el-table-column>
+    <el-table-column prop="Basesalary" label="底薪" width="100">
+    </el-table-column>
+    <el-table-column prop="Wx_achieve" label="纹绣业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Pf_achieve" label="皮肤业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Zc_achieve" label="专场业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Gd_achieve" label="光电业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Bs_achieve" label="补色业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Shop_achieve" label="门店业绩" width="100">
+    </el-table-column>
+    <el-table-column prop="Wxpf_commission" label="纹绣/皮肤提成" width="100">
+    </el-table-column>
+    <el-table-column prop="Zc_commission" label="专场提成" width="100">
+    </el-table-column>
+    <el-table-column prop="Gd_commission" label="光电提成" width="100">
+    </el-table-column>
+    <el-table-column prop="Bs_commission" label="补色提成" width="100">
+    </el-table-column>
+    <el-table-column prop="shop_commission" label="门店提成" width="100">
+    </el-table-column>
+    <el-table-column prop="Achievement" label="绩效" width="100">
+    </el-table-column>
+    <el-table-column prop="Subsidy" label="补贴" width="100">
+    </el-table-column>
+    <el-table-column prop="Handcost" label="手工费" width="100">
+    </el-table-column>
+    <el-table-column prop="Ac_basesalary" label="实发底薪" width="100">
+    </el-table-column>
+    <el-table-column prop="Deduction" label="考勤扣款" width="100">
+    </el-table-column>
+    <el-table-column prop="Month_salary" label="本月工资合计" width="100">
+    </el-table-column>
+  </el-table>
+</template>
 
 <script>
-export default{
-    name :"AchieveSearch",
-    data(){
-        return {
-            tableData:[],
-        }
-    },
-    mounted(){
-        this.initSyAchieveInfo();
-    },
-    methods:{
-        initSyAchieveInfo(){
-            this.getRequest("/api/achieve/sy/info/search").then(resp=>{
-                if(resp){
-                    var arr = []
-                    arr.push(resp.data)
-                    this.tableData = arr
-                }
-            })
-        }
+export default {
+  name: "AchieveSearch",
+  data() {
+    return {
+      tableData: [],
     }
+  },
+  mounted() {
+    this.initSyAchieveInfo();
+  },
+  methods: {
+    initSyAchieveInfo() {
+      this.getRequest("/api/achieve/sy/info/search").then(resp => {
+        if (resp) {
+          var arr = []
+          for (var i = 0; i < resp.data.length; i++) {
+            arr.push(resp.data[i])
+
+          }
+          this.tableData = arr
+          // console.log("tabledata:",this.tableData)
+        }
+      })
+    }
+  }
 
 
 }
--- yeb.sql
+++ yeb.sql
@@ -75,6 +75,153 @@ INSERT INTO `t_admin_role` VALUES (56, 3, 4);
 INSERT INTO `t_admin_role` VALUES (67, 2, 3);
 INSERT INTO `t_admin_role` VALUES (68, 2, 4);
 
+
+-- ----------------------------
+-- Table structure for t_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
+  `nameZh` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES (1, 'ROLE_manager', '部门经理');
+INSERT INTO `t_role` VALUES (2, 'ROLE_personnel', '人事专员');
+INSERT INTO `t_role` VALUES (3, 'ROLE_recruiter', '招聘主管');
+INSERT INTO `t_role` VALUES (4, 'ROLE_train', '培训主管');
+INSERT INTO `t_role` VALUES (5, 'ROLE_performance', '薪酬绩效主管');
+INSERT INTO `t_role` VALUES (6, 'ROLE_admin', '系统管理员');
+INSERT INTO `t_role` VALUES (8, 'ROLE_test', '测试角色');
+
+-- ----------------------------
+-- Table structure for t_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `t_menu`;
+CREATE TABLE `t_menu`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `url` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'url',
+  `path` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'path',
+  `component` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '组件',
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单名',
+  `iconCls` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
+  `keepAlive` tinyint(1) NULL DEFAULT NULL COMMENT '是否保持激活',
+  `requireAuth` tinyint(1) NULL DEFAULT NULL COMMENT '是否要求权限',
+  `parentId` int(11) NULL DEFAULT NULL COMMENT '父id',
+  `enabled` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `parentId`(`parentId`) USING BTREE,
+  CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`parentId`) REFERENCES `t_menu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_menu
+-- ----------------------------
+INSERT INTO `t_menu` VALUES (1, '/', NULL, NULL, '所有', NULL, NULL, NULL, NULL, 1);
+INSERT INTO `t_menu` VALUES (2, '/', '/home', 'Home', '员工资料', 'fa fa-user-circle-o', NULL, 1, 1, 1);
+INSERT INTO `t_menu` VALUES (3, '/', '/home', 'Home', '人事管理', 'fa fa-address-card-o', NULL, 1, 1, 1);
+INSERT INTO `t_menu` VALUES (4, '/', '/home', 'Home', '薪资管理', 'fa fa-money', NULL, 1, 1, 1);
+INSERT INTO `t_menu` VALUES (5, '/', '/home', 'Home', '统计管理', 'fa fa-bar-chart', NULL, 1, 1, 1);
+INSERT INTO `t_menu` VALUES (6, '/', '/home', 'Home', '系统管理', 'fa fa-windows', NULL, 1, 1, 1);
+INSERT INTO `t_menu` VALUES (7, '/employee/basic/**', '/emp/basic', 'EmpBasic', '基本资料', NULL, NULL, 1, 2, 1);
+INSERT INTO `t_menu` VALUES (8, '/employee/advanced/**', '/emp/adv', 'EmpAdv', '高级资料', NULL, NULL, 1, 2, 1);
+INSERT INTO `t_menu` VALUES (9, '/personnel/emp/**', '/per/emp', 'PerEmp', '员工资料', NULL, NULL, 1, 3, 1);
+INSERT INTO `t_menu` VALUES (10, '/personnel/ec/**', '/per/ec', 'PerEc', '员工奖惩', NULL, NULL, 1, 3, 1);
+INSERT INTO `t_menu` VALUES (11, '/personnel/train/**', '/per/train', 'PerTrain', '员工培训', NULL, NULL, 1, 3, 1);
+INSERT INTO `t_menu` VALUES (12, '/personnel/salary/**', '/per/salary', 'PerSalary', '员工调薪', NULL, NULL, 1, 3, 1);
+INSERT INTO `t_menu` VALUES (13, '/personnel/remove/**', '/per/mv', 'PerMv', '员工调动', NULL, NULL, 1, 3, 1);
+INSERT INTO `t_menu` VALUES (14, '/salary/sob/**', '/sal/sob', 'SalSob', '工资账套管理', NULL, NULL, 1, 4, 1);
+INSERT INTO `t_menu` VALUES (15, '/salary/sobcfg/**', '/sal/sobcfg', 'SalSobCfg', '员工账套设置', NULL, NULL, 1, 4, 1);
+INSERT INTO `t_menu` VALUES (16, '/salary/table/**', '/sal/table', 'SalTable', '工资表管理', NULL, NULL, 1, 4, 1);
+INSERT INTO `t_menu` VALUES (17, '/salary/month/**', '/sal/month', 'SalMonth', '月末处理', NULL, NULL, 1, 4, 1);
+INSERT INTO `t_menu` VALUES (18, '/salary/search/**', '/sal/search', 'SalSearch', '工资表查询', NULL, NULL, 1, 4, 1);
+INSERT INTO `t_menu` VALUES (19, '/statistics/all/**', '/sta/all', 'StaAll', '综合信息统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `t_menu` VALUES (20, '/statistics/score/**', '/sta/score', 'StaScore', '员工积分统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `t_menu` VALUES (21, '/statistics/personnel/**', '/sta/pers', 'StaPers', '人事信息统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `t_menu` VALUES (22, '/statistics/recored/**', '/sta/record', 'StaRecord', '人事记录统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `t_menu` VALUES (23, '/system/basic/**', '/sys/basic', 'SysBasic', '基础信息设置', NULL, NULL, 1, 6, 1);
+INSERT INTO `t_menu` VALUES (24, '/system/cfg/**', '/sys/cfg', 'SysCfg', '系统管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `t_menu` VALUES (25, '/system/log/**', '/sys/log', 'SysLog', '操作日志管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `t_menu` VALUES (26, '/system/admin/**', '/sys/admin', 'SysAdmin', '操作员管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `t_menu` VALUES (27, '/system/data/**', '/sys/data', 'SysData', '备份恢复数据库', NULL, NULL, 1, 6, 1);
+INSERT INTO `t_menu` VALUES (28, '/system/init/**', '/sys/init', 'SysInit', '初始化数据库', NULL, NULL, 1, 6, 1);
+
+-- ----------------------------
+-- Table structure for t_menu_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_menu_role`;
+CREATE TABLE `t_menu_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `mid` int(11) NULL DEFAULT NULL COMMENT '菜单id',
+  `rid` int(11) NULL DEFAULT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `mid`(`mid`) USING BTREE,
+  INDEX `rid`(`rid`) USING BTREE,
+  CONSTRAINT `t_menu_role_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `t_menu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `t_menu_role_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `t_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 747 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_menu_role
+-- ----------------------------
+INSERT INTO `t_menu_role` VALUES (1, 7, 3);
+INSERT INTO `t_menu_role` VALUES (2, 7, 6);
+INSERT INTO `t_menu_role` VALUES (3, 9, 6);
+INSERT INTO `t_menu_role` VALUES (4, 10, 6);
+INSERT INTO `t_menu_role` VALUES (5, 11, 6);
+INSERT INTO `t_menu_role` VALUES (6, 12, 6);
+INSERT INTO `t_menu_role` VALUES (7, 13, 6);
+INSERT INTO `t_menu_role` VALUES (8, 14, 6);
+INSERT INTO `t_menu_role` VALUES (9, 15, 6);
+INSERT INTO `t_menu_role` VALUES (10, 16, 6);
+INSERT INTO `t_menu_role` VALUES (11, 17, 6);
+INSERT INTO `t_menu_role` VALUES (12, 18, 6);
+INSERT INTO `t_menu_role` VALUES (13, 19, 6);
+INSERT INTO `t_menu_role` VALUES (14, 20, 6);
+INSERT INTO `t_menu_role` VALUES (15, 21, 6);
+INSERT INTO `t_menu_role` VALUES (16, 22, 6);
+INSERT INTO `t_menu_role` VALUES (17, 23, 6);
+INSERT INTO `t_menu_role` VALUES (18, 25, 6);
+INSERT INTO `t_menu_role` VALUES (19, 26, 6);
+INSERT INTO `t_menu_role` VALUES (20, 27, 6);
+INSERT INTO `t_menu_role` VALUES (21, 28, 6);
+INSERT INTO `t_menu_role` VALUES (22, 24, 6);
+INSERT INTO `t_menu_role` VALUES (26, 7, 2);
+INSERT INTO `t_menu_role` VALUES (27, 8, 2);
+INSERT INTO `t_menu_role` VALUES (28, 9, 2);
+INSERT INTO `t_menu_role` VALUES (29, 10, 2);
+INSERT INTO `t_menu_role` VALUES (30, 12, 2);
+INSERT INTO `t_menu_role` VALUES (31, 13, 2);
+INSERT INTO `t_menu_role` VALUES (32, 7, 1);
+INSERT INTO `t_menu_role` VALUES (33, 8, 1);
+INSERT INTO `t_menu_role` VALUES (34, 9, 1);
+INSERT INTO `t_menu_role` VALUES (35, 10, 1);
+INSERT INTO `t_menu_role` VALUES (36, 11, 1);
+INSERT INTO `t_menu_role` VALUES (37, 12, 1);
+INSERT INTO `t_menu_role` VALUES (38, 13, 1);
+INSERT INTO `t_menu_role` VALUES (39, 14, 1);
+INSERT INTO `t_menu_role` VALUES (40, 15, 1);
+INSERT INTO `t_menu_role` VALUES (41, 16, 1);
+INSERT INTO `t_menu_role` VALUES (42, 17, 1);
+INSERT INTO `t_menu_role` VALUES (43, 18, 1);
+INSERT INTO `t_menu_role` VALUES (44, 19, 1);
+INSERT INTO `t_menu_role` VALUES (45, 20, 1);
+INSERT INTO `t_menu_role` VALUES (46, 21, 1);
+INSERT INTO `t_menu_role` VALUES (47, 22, 1);
+INSERT INTO `t_menu_role` VALUES (48, 23, 1);
+INSERT INTO `t_menu_role` VALUES (49, 24, 1);
+INSERT INTO `t_menu_role` VALUES (50, 25, 1);
+INSERT INTO `t_menu_role` VALUES (51, 26, 1);
+INSERT INTO `t_menu_role` VALUES (52, 27, 1);
+INSERT INTO `t_menu_role` VALUES (53, 28, 1);
+INSERT INTO `t_menu_role` VALUES (346, 11, 4);
+INSERT INTO `t_menu_role` VALUES (347, 8, 4);
+INSERT INTO `t_menu_role` VALUES (348, 7, 4);
+
 -- ----------------------------
 -- Table structure for t_appraise
 -- ----------------------------
@@ -367,129 +514,7 @@ CREATE TABLE `t_mail_log`  (
   UNIQUE INDEX `msgId`(`msgId`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Table structure for t_menu
--- ----------------------------
-DROP TABLE IF EXISTS `t_menu`;
-CREATE TABLE `t_menu`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `url` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'url',
-  `path` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'path',
-  `component` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '组件',
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '菜单名',
-  `iconCls` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图标',
-  `keepAlive` tinyint(1) NULL DEFAULT NULL COMMENT '是否保持激活',
-  `requireAuth` tinyint(1) NULL DEFAULT NULL COMMENT '是否要求权限',
-  `parentId` int(11) NULL DEFAULT NULL COMMENT '父id',
-  `enabled` tinyint(1) NULL DEFAULT 1 COMMENT '是否启用',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `parentId`(`parentId`) USING BTREE,
-  CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`parentId`) REFERENCES `t_menu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of t_menu
--- ----------------------------
-INSERT INTO `t_menu` VALUES (1, '/', NULL, NULL, '所有', NULL, NULL, NULL, NULL, 1);
-INSERT INTO `t_menu` VALUES (2, '/', '/home', 'Home', '员工资料', 'fa fa-user-circle-o', NULL, 1, 1, 1);
-INSERT INTO `t_menu` VALUES (3, '/', '/home', 'Home', '人事管理', 'fa fa-address-card-o', NULL, 1, 1, 1);
-INSERT INTO `t_menu` VALUES (4, '/', '/home', 'Home', '薪资管理', 'fa fa-money', NULL, 1, 1, 1);
-INSERT INTO `t_menu` VALUES (5, '/', '/home', 'Home', '统计管理', 'fa fa-bar-chart', NULL, 1, 1, 1);
-INSERT INTO `t_menu` VALUES (6, '/', '/home', 'Home', '系统管理', 'fa fa-windows', NULL, 1, 1, 1);
-INSERT INTO `t_menu` VALUES (7, '/employee/basic/**', '/emp/basic', 'EmpBasic', '基本资料', NULL, NULL, 1, 2, 1);
-INSERT INTO `t_menu` VALUES (8, '/employee/advanced/**', '/emp/adv', 'EmpAdv', '高级资料', NULL, NULL, 1, 2, 1);
-INSERT INTO `t_menu` VALUES (9, '/personnel/emp/**', '/per/emp', 'PerEmp', '员工资料', NULL, NULL, 1, 3, 1);
-INSERT INTO `t_menu` VALUES (10, '/personnel/ec/**', '/per/ec', 'PerEc', '员工奖惩', NULL, NULL, 1, 3, 1);
-INSERT INTO `t_menu` VALUES (11, '/personnel/train/**', '/per/train', 'PerTrain', '员工培训', NULL, NULL, 1, 3, 1);
-INSERT INTO `t_menu` VALUES (12, '/personnel/salary/**', '/per/salary', 'PerSalary', '员工调薪', NULL, NULL, 1, 3, 1);
-INSERT INTO `t_menu` VALUES (13, '/personnel/remove/**', '/per/mv', 'PerMv', '员工调动', NULL, NULL, 1, 3, 1);
-INSERT INTO `t_menu` VALUES (14, '/salary/sob/**', '/sal/sob', 'SalSob', '工资账套管理', NULL, NULL, 1, 4, 1);
-INSERT INTO `t_menu` VALUES (15, '/salary/sobcfg/**', '/sal/sobcfg', 'SalSobCfg', '员工账套设置', NULL, NULL, 1, 4, 1);
-INSERT INTO `t_menu` VALUES (16, '/salary/table/**', '/sal/table', 'SalTable', '工资表管理', NULL, NULL, 1, 4, 1);
-INSERT INTO `t_menu` VALUES (17, '/salary/month/**', '/sal/month', 'SalMonth', '月末处理', NULL, NULL, 1, 4, 1);
-INSERT INTO `t_menu` VALUES (18, '/salary/search/**', '/sal/search', 'SalSearch', '工资表查询', NULL, NULL, 1, 4, 1);
-INSERT INTO `t_menu` VALUES (19, '/statistics/all/**', '/sta/all', 'StaAll', '综合信息统计', NULL, NULL, 1, 5, 1);
-INSERT INTO `t_menu` VALUES (20, '/statistics/score/**', '/sta/score', 'StaScore', '员工积分统计', NULL, NULL, 1, 5, 1);
-INSERT INTO `t_menu` VALUES (21, '/statistics/personnel/**', '/sta/pers', 'StaPers', '人事信息统计', NULL, NULL, 1, 5, 1);
-INSERT INTO `t_menu` VALUES (22, '/statistics/recored/**', '/sta/record', 'StaRecord', '人事记录统计', NULL, NULL, 1, 5, 1);
-INSERT INTO `t_menu` VALUES (23, '/system/basic/**', '/sys/basic', 'SysBasic', '基础信息设置', NULL, NULL, 1, 6, 1);
-INSERT INTO `t_menu` VALUES (24, '/system/cfg/**', '/sys/cfg', 'SysCfg', '系统管理', NULL, NULL, 1, 6, 1);
-INSERT INTO `t_menu` VALUES (25, '/system/log/**', '/sys/log', 'SysLog', '操作日志管理', NULL, NULL, 1, 6, 1);
-INSERT INTO `t_menu` VALUES (26, '/system/admin/**', '/sys/admin', 'SysAdmin', '操作员管理', NULL, NULL, 1, 6, 1);
-INSERT INTO `t_menu` VALUES (27, '/system/data/**', '/sys/data', 'SysData', '备份恢复数据库', NULL, NULL, 1, 6, 1);
-INSERT INTO `t_menu` VALUES (28, '/system/init/**', '/sys/init', 'SysInit', '初始化数据库', NULL, NULL, 1, 6, 1);
-
--- ----------------------------
--- Table structure for t_menu_role
--- ----------------------------
-DROP TABLE IF EXISTS `t_menu_role`;
-CREATE TABLE `t_menu_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `mid` int(11) NULL DEFAULT NULL COMMENT '菜单id',
-  `rid` int(11) NULL DEFAULT NULL COMMENT '权限id',
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `mid`(`mid`) USING BTREE,
-  INDEX `rid`(`rid`) USING BTREE,
-  CONSTRAINT `t_menu_role_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `t_menu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `t_menu_role_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `t_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 747 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_menu_role
--- ----------------------------
-INSERT INTO `t_menu_role` VALUES (1, 7, 3);
-INSERT INTO `t_menu_role` VALUES (2, 7, 6);
-INSERT INTO `t_menu_role` VALUES (3, 9, 6);
-INSERT INTO `t_menu_role` VALUES (4, 10, 6);
-INSERT INTO `t_menu_role` VALUES (5, 11, 6);
-INSERT INTO `t_menu_role` VALUES (6, 12, 6);
-INSERT INTO `t_menu_role` VALUES (7, 13, 6);
-INSERT INTO `t_menu_role` VALUES (8, 14, 6);
-INSERT INTO `t_menu_role` VALUES (9, 15, 6);
-INSERT INTO `t_menu_role` VALUES (10, 16, 6);
-INSERT INTO `t_menu_role` VALUES (11, 17, 6);
-INSERT INTO `t_menu_role` VALUES (12, 18, 6);
-INSERT INTO `t_menu_role` VALUES (13, 19, 6);
-INSERT INTO `t_menu_role` VALUES (14, 20, 6);
-INSERT INTO `t_menu_role` VALUES (15, 21, 6);
-INSERT INTO `t_menu_role` VALUES (16, 22, 6);
-INSERT INTO `t_menu_role` VALUES (17, 23, 6);
-INSERT INTO `t_menu_role` VALUES (18, 25, 6);
-INSERT INTO `t_menu_role` VALUES (19, 26, 6);
-INSERT INTO `t_menu_role` VALUES (20, 27, 6);
-INSERT INTO `t_menu_role` VALUES (21, 28, 6);
-INSERT INTO `t_menu_role` VALUES (22, 24, 6);
-INSERT INTO `t_menu_role` VALUES (26, 7, 2);
-INSERT INTO `t_menu_role` VALUES (27, 8, 2);
-INSERT INTO `t_menu_role` VALUES (28, 9, 2);
-INSERT INTO `t_menu_role` VALUES (29, 10, 2);
-INSERT INTO `t_menu_role` VALUES (30, 12, 2);
-INSERT INTO `t_menu_role` VALUES (31, 13, 2);
-INSERT INTO `t_menu_role` VALUES (32, 7, 1);
-INSERT INTO `t_menu_role` VALUES (33, 8, 1);
-INSERT INTO `t_menu_role` VALUES (34, 9, 1);
-INSERT INTO `t_menu_role` VALUES (35, 10, 1);
-INSERT INTO `t_menu_role` VALUES (36, 11, 1);
-INSERT INTO `t_menu_role` VALUES (37, 12, 1);
-INSERT INTO `t_menu_role` VALUES (38, 13, 1);
-INSERT INTO `t_menu_role` VALUES (39, 14, 1);
-INSERT INTO `t_menu_role` VALUES (40, 15, 1);
-INSERT INTO `t_menu_role` VALUES (41, 16, 1);
-INSERT INTO `t_menu_role` VALUES (42, 17, 1);
-INSERT INTO `t_menu_role` VALUES (43, 18, 1);
-INSERT INTO `t_menu_role` VALUES (44, 19, 1);
-INSERT INTO `t_menu_role` VALUES (45, 20, 1);
-INSERT INTO `t_menu_role` VALUES (46, 21, 1);
-INSERT INTO `t_menu_role` VALUES (47, 22, 1);
-INSERT INTO `t_menu_role` VALUES (48, 23, 1);
-INSERT INTO `t_menu_role` VALUES (49, 24, 1);
-INSERT INTO `t_menu_role` VALUES (50, 25, 1);
-INSERT INTO `t_menu_role` VALUES (51, 26, 1);
-INSERT INTO `t_menu_role` VALUES (52, 27, 1);
-INSERT INTO `t_menu_role` VALUES (53, 28, 1);
-INSERT INTO `t_menu_role` VALUES (346, 11, 4);
-INSERT INTO `t_menu_role` VALUES (347, 8, 4);
-INSERT INTO `t_menu_role` VALUES (348, 7, 4);
 
 -- ----------------------------
 -- Table structure for t_nation
@@ -624,27 +649,7 @@ INSERT INTO `t_position` VALUES (3, '市场总监', '2020-03-31 16:20:34', 1);
 INSERT INTO `t_position` VALUES (4, '研发工程师', '2020-03-31 16:20:34', 1);
 INSERT INTO `t_position` VALUES (5, '运维工程师', '2020-03-31 16:20:34', 1);
 
--- ----------------------------
--- Table structure for t_role
--- ----------------------------
-DROP TABLE IF EXISTS `t_role`;
-CREATE TABLE `t_role`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '名称',
-  `nameZh` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名称',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of t_role
--- ----------------------------
-INSERT INTO `t_role` VALUES (1, 'ROLE_manager', '部门经理');
-INSERT INTO `t_role` VALUES (2, 'ROLE_personnel', '人事专员');
-INSERT INTO `t_role` VALUES (3, 'ROLE_recruiter', '招聘主管');
-INSERT INTO `t_role` VALUES (4, 'ROLE_train', '培训主管');
-INSERT INTO `t_role` VALUES (5, 'ROLE_performance', '薪酬绩效主管');
-INSERT INTO `t_role` VALUES (6, 'ROLE_admin', '系统管理员');
-INSERT INTO `t_role` VALUES (8, 'ROLE_test', '测试角色');
 
 -- ----------------------------
 -- Table structure for t_salary
